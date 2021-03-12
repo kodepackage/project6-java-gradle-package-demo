9c7c28ca9fa4474f07d41009948a6812f3fc2603
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-12 21:52:16 +0530

    initial commit.

---
+++ src/main/java/in/koderoom/HelloWorld.java
@@ -0,0 +1,12 @@
+package in.koderoom;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(helloGradle());
+    }
+
+    public static String helloGradle() {
+        return "Hello Gradle";
+    }
+}
